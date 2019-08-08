28321945fe6cfe8499fd648d134b9357135ca0c7
thgilcp <thgilcp@gmail.com>
2019-08-08 12:00:17 +0900

    Hide Avatars, Usernames and Anchors.

---
+++ script.js
@@ -0,0 +1,16 @@
+let elm = document.querySelectorAll('div[class*="avatar-"]');
+elm.forEach(e => (e.querySelector("svg").style.display = "none"));
+elm.forEach(e => (e.style.background = "black"));
+elm.forEach(e => (e.style.borderRadius = "50%"));
+elm = document.querySelectorAll('span[class*="username-"]');
+elm.forEach(e => (e.style.color = "black"));
+elm.forEach(e => (e.style.background = "black"));
+elm.forEach(e => (e.style.borderRadius = "5px"));
+elm = document.querySelectorAll('span[class*="mention"]');
+elm.forEach(e => (e.style.color = "black"));
+elm.forEach(e => (e.style.background = "black"));
+elm.forEach(e => (e.style.borderRadius = "5px"));
+elm = document.querySelectorAll('a[class*="anchor-"]');
+elm.forEach(e => (e.style.color = "black"));
+elm.forEach(e => (e.style.background = "black"));
+elm.forEach(e => (e.style.borderRadius = "5px"));
